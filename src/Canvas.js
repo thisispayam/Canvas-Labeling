2b248bc89fb784213ada62144620dc967f2231c2
Payam <thisispayam@gmail.com>
2023-02-20 20:57:52 -0800

    Added div outside of the canvas to represent the selections inside the canvas.

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,11 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
 
-const CANVAS_WIDTH = 500;
+const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 500;
 
 function Canvas() {
   const [rectangles, setRectangles] = useState([]);
-  const [selected, setSelected] = useState(-1);
+  const [selected, setSelected] = useState({ rectangle: -1, div: -1 });
+
   const [canvasReady, setCanvasReady] = useState(false);
   const [drawingRect, setDrawingRect] = useState(null);
 
@@ -28,6 +29,22 @@ function Canvas() {
       x: e.nativeEvent.offsetX,
       y: e.nativeEvent.offsetY
     };
+
+    const clickedRectIndex = rectangles.findIndex(rect => {
+      const rectLeft = rect.x;
+      const rectTop = rect.y;
+      const rectRight = rect.x + rect.width;
+      const rectBottom = rect.y + rect.height;
+      return mouseRef.current.x >= rectLeft && mouseRef.current.x <= rectRight && mouseRef.current.y >= rectTop && mouseRef.current.y <= rectBottom;
+    });
+
+    if (clickedRectIndex !== -1) {
+      setSelected(clickedRectIndex);
+      const rectDiv = document.getElementById(`rectDiv-${clickedRectIndex}`);
+      if (rectDiv) {
+        rectDiv.style.border = "2px solid orange";
+      }
+    }
   };
 
   const handleMouseMove = e => {
@@ -75,11 +92,21 @@ function Canvas() {
 
   const handleRectClick = index => {
     setSelected(index);
+    const rectDiv = document.getElementById(`rectDiv-${index}`);
+    if (rectDiv) {
+      rectDiv.style.border = "2px solid orange";
+    }
   };
 
   const handleRectClose = index => {
     setRectangles(rectangles.filter((_, i) => i !== index));
-    setSelected(-1);
+    if (selected === index) {
+      setSelected(-1);
+    }
+    const rectDiv = document.getElementById(`rectDiv-${index}`);
+    if (rectDiv) {
+      rectDiv.style.border = "2px solid black";
+    }
   };
 
   const handleTextChange = (index, event) => {
@@ -99,19 +126,21 @@ function Canvas() {
 
   return (
     <div style={{ position: "relative" }}>
-      <canvas ref={canvasRef} width={CANVAS_WIDTH} height={CANVAS_HEIGHT} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp} />
+      <canvas ref={canvasRef} width={CANVAS_WIDTH} height={CANVAS_HEIGHT} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp} style={{ zIndex: 0 }} />
       {canvasReady && (
         <>
           {rectangles.map((rect, i) => (
             <div
               key={i}
+              id={`rectDiv-${i}`}
               style={{
                 position: "absolute",
+                zIndex: 1,
                 top: rect.y,
                 left: rect.x,
                 width: rect.width,
                 height: rect.height,
-                border: selected === i ? "2px solid yellow" : "2px solid black",
+                border: selected === i ? "2px solid orange" : "2px solid black",
                 cursor: "move"
               }}
               onClick={() => handleRectClick(i)}
@@ -119,16 +148,18 @@ function Canvas() {
               <div
                 style={{
                   display: "flex",
+                  position: "absolute",
+                  top: "-29px",
+                  left: "-2px",
                   justifyContent: "space-between",
                   alignItems: "center",
                   padding: "4px",
-                  backgroundColor: "white",
-                  borderTop: "1px solid black",
-                  borderBottom: "1px solid black"
+                  backgroundColor: "rgba(0,0,0,0.5)",
+                  color: "#fff"
                 }}
               >
                 <div>{i + 1}</div>
-                <div style={{ cursor: "pointer" }} onClick={() => handleRectClose(i)}>
+                <div style={{ cursor: "pointer", marginLeft: "20px" }} onClick={() => handleRectClose(i)}>
                   x
                 </div>
               </div>
@@ -155,10 +186,17 @@ function Canvas() {
                 left: drawingRect.x,
                 width: drawingRect.width,
                 height: drawingRect.height,
-                border: "2px dashed black"
+                border: "2px dashed grey"
               }}
             />
           )}
+          {rectangles.map((rect, i) => (
+            <div key={i} onClick={() => handleRectClick(i)}>
+              <div>{i + 1}</div>
+              <input type="text" value={rect.text} onChange={event => handleTextChange(i, event)} />
+              <button onClick={() => handleRectClose(i)}>x</button>
+            </div>
+          ))}
         </>
       )}
     </div>
