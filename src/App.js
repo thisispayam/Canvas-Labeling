6df9d9f7f9946172846ba6a41cc959b93880c92b
Payam <thisispayam@gmail.com>
2023-02-23 21:25:36 -0800

    description update

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ function App() {
       <Canvas />
 
       <div className="desc">
-        <p> POC of drawing rectangle polygons on a Canvas. Each polygon comes with a number field on top. It only accepts 2-digit numbers.</p>
-        <p> Each rectangle will generate another field on the right-side panel which the number labels for both are synced.</p>
+        <p>POC of drawing rectangle polygons on a Canvas. Each polygon comes with a number field on top. It only accepts 2-digit numbers.</p>
+        <p>Each rectangle will generate another field on the right-side panel which the number labels for both are synced.</p>
         <p>You can also write a short description for each rectangle on the side panel. </p>
+        <p>You can drag and drop them anywhere in the canvas.</p>
         <p>You can delete the rectangle either by clicking on the "x" on top of it or from the side panel.</p>
       </div>
     </div>
